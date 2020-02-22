dba38241949ed3358cd453652af8553061662bc5
Benjamin Killer <benji_killer@hotmail.de>
2020-02-22 16:36:34 +0100

    add text

--- pages/defaultSite.js
+++ pages/defaultSite.js
@@ -5,8 +5,8 @@ const Index = () => {
         <div>
             <h1>Hello</h1>
             <p>Hier entsteht meine Webseite</p>
-            <tr />
             <p>Bei langeweile kannst du gerne die Webseite meiner wundervollen Frau besuchen: <a target="_blank" href="https://www.kwerbeed.de">www.kwerbeed.de</a></p>
+            <p>otherwise - take a nap</p>
         </div>
     )
 }
